d877491ef436036c0155e56a343862705b67ff8c
Ming <mxu925@gmail.com>
2020-07-07 13:46:33 +1000

    Added automatic download of raw INS in setup script

--- setup/setup.sh
+++ setup/setup.sh
@@ -3,3 +3,12 @@ echo "DATA_PATH = '$dir'" >> ./hfnet/settings.py
 
 read -p "Path of the directory where experiments data (logs, checkpoints, configs) are written: " dir
 echo "EXPER_PATH = '$dir'" >> ./hfnet/settings.py
+
+read -p "Path of the directory containing the original RobotCar traverse data: " dir
+echo "RAW_PATH = '$dir'" >> ./hfnet/settings.py
+
+read -p "Insert RobotCar dataset website username: " name
+read -p "Insert website password: " pword
+
+echo pwd
+python ./thirdparty/RobotCarDataset-Scraper-master/scrape_mrgdatashare.py --downloads_dir $dir --datasets_file ./thirdparty/RobotCarDataset-Scraper-master/vloc_challenges.csv --username $name --password $pword
